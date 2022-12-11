d770ca09cd8a73d7b7fe625420c7ca7eeae90064
Abdul300634 <hanan15082002@gmail.com>
2022-12-12 00:06:24 +0500

    up

--- SocketContext.js
+++ SocketContext.js
@@ -18,7 +18,9 @@ import Peer from "simple-peer";
 const SocketContext = createContext();
 
 // const socket = io("http://localhost:5000");
-const socket = io("https://consultproapi.herokuapp.com");
+const socket = io("https://consultproapi.herokuapp.com", {
+  transports: ["websocket"],
+});
 // const socket = io("https://warm-wildwood-81069.herokuapp.com");
 
 const ContextProvider = ({ children }) => {
@@ -34,7 +36,6 @@ const ContextProvider = ({ children }) => {
   const myVideo = useRef();
   const userVideo = useRef();
   const connectionRef = useRef();
-  console.log();
 
   useEffect(() => {
     navigator.mediaDevices
